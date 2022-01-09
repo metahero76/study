e70be7e49bc12862363e0f0afda8119a3ddf5867
metahero76 <96629767+metahero76@users.noreply.github.com>
2022-01-10 02:10:59 +0900

    Add files via upload
    study

---
+++ study/webflux-20220110/src/main/java/zz/Webflux20220110Application.java
@@ -0,0 +1,13 @@
+package zz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webflux20220110Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Webflux20220110Application.class, args);
+    }
+
+}
