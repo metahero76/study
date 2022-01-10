4a9e962756774ee2f7d0a3bac396f0f35c6c3bc2
metahero76 <96629767+metahero76@users.noreply.github.com>
2022-01-11 00:10:30 +0900

    init
    init

---
+++ reactive_streams/src/main/java/zz/ReactiveStreams01Application.java
@@ -0,0 +1,18 @@
+package zz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveStreams01Application {
+    //Main
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveStreams01Application.class, args);
+
+        MyPub pub = new MyPub();//신문사 생성
+        MySub sub = new MySub();//구독자 생성
+
+        pub.subscribe(sub);
+    }
+
+}
