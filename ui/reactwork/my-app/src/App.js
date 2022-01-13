e5a945e45ad8d59f14e6d593ec4b69a80e8dda91
metahero <alche76@naver.com>
2022-01-13 20:30:34 +0900

    react 바인딩

--- ui/reactwork/my-app/src/App.js
+++ ui/reactwork/my-app/src/App.js
@@ -4,21 +4,28 @@ import React, { useEffect, useState } from "react";
 
 function App() {
 
-  //let number = 1; // 상태 값 아님
-  // eslint-disable-next-line no-undef
-  const [nn, setNn] = useState(2);
+  console.log("App실행됨");
 
+  let sample = [
+    {id:1, name:"홍길동"},
+    {id:2, name:"임꺽정"},
+    {id:3, name:"장보고"},
+    {id:4, name:"코스"}
+  ];
 
-  const add = () => {
-    setNn(nn + 1);
-    console.log('add', nn);
+  const [users, setUsers] = useState(sample);
+
+  const download = () => {
+    //setUsers([...sample]);//데이터가 같아도 레퍼런스 주소가 다르기 때문에 다시그려짐
+    //setUsers(sample);
   }
 
   return(
     <div>
       <div>
-        <h1>숫자 : {nn}</h1>
-        <button onClick={add}>더하기</button>
+        <h1></h1>
+        <button onClick={download}>다운로드</button>
+        {users.map((u, index)=><h1>{u.id}, {u.name}</h1>)}
       </div>
     </div>
   );
