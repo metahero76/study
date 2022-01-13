ec43b0b59f189695b14467f9cbc1c729cb78d23e
metahero <alche76@naver.com>
2022-01-13 17:12:49 +0900

    react 바인딩 useState변수

--- ui/reactwork/my-app/src/App.js
+++ ui/reactwork/my-app/src/App.js
@@ -1,20 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
-  
-  let list = [1,2,3];
+
+  //let number = 1; // 상태 값 아님
+  // eslint-disable-next-line no-undef
+  const [nn, setNn] = useState(2);
+
+
+  const add = () => {
+    setNn(nn + 1);
+    console.log('add', nn);
+  }
 
   return(
     <div>
       <div>
-        {list.map((n, index) => (
-          <h1 key={index}>{n}</h1>
-        ))}
+        <h1>숫자 : {nn}</h1>
+        <button onClick={add}>더하기</button>
       </div>
     </div>
   );
-
+ 
 }
 
 export default App;
